37d95277addb82439f8d2b2b536aa691ca11f595
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-01-21 19:36:31 +0100

    Veritas 2

--- src/main/scala/test/TestExample.scala
+++ src/main/scala/test/TestExample.scala
@@ -4,12 +4,21 @@ import scala.language.postfixOps
 
 package test {
 
+  import veritas.PoSharpScript
   import veritas.Veritas.GetOutput
 
   @Test
   class TestExample {
-    def runTest(): Boolean = {
-      "2" == GetOutput("{def a; a = 5; print(a);}")
+    def runTest2(): (Boolean, String) = {
+      new PoSharpScript("{def a; a = 5; print(a);}")
+        .ShouldBe("5")
+        .Run()
+    }
+
+    def runTest3(): (Boolean, String) = {
+      new PoSharpScript("{def a; a = 5; print(a);}")
+        .ShouldBe("6")
+        .Run()
     }
   }
 }
